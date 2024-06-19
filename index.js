19a317ef72b5138a78706ea43760011f8b678766
Alfian Maulana <alfianmna@gmail.com>
2024-06-20 00:03:32 +0700

    cahnge

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 const corsOption = {
-  origin: ["https://kiost.netlify.app", "https://kiost.netlify.app/", "https://kiost.vercel.app/", "http://localhost:5173", "http://localhost:3000", "http://localhost:4000"],
+  origin: ["https://kiost.netlify.app", "https://kiost.vercel.app", "http://localhost:5173", "http://localhost:3000", "http://localhost:4000"],
   credentials: true,
 };
 
 app.use(cors(corsOption));
+
 // Endpoint to save data to data.json
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
 app.post("/save-data", (req, res) => {
   const newData = req.body;
-  const file = path.join(__dirname, "../public/script/data.json");
-  console.log("Received data:", newData);
+  const fileUrl = "https://kiost.netlify.app/script/data.json"; // New URL
 
-  fs.readFile(file, (err, data) => {
+  // Read the data from the remote URL
+  fs.readFile(fileUrl, (err, data) => {
     if (err) {
       console.error("Failed to read data.json:", err);
       res.status(500).send("Failed to save data");
@@ -34,7 +36,8 @@ app.post("/save-data", (req, res) => {
     let jsonData = JSON.parse(data);
     jsonData.push(newData);
 
-    fs.writeFile(file, JSON.stringify(jsonData, null, 2), (err) => {
+    // Write the updated data back to the remote URL
+    fs.writeFile(fileUrl, JSON.stringify(jsonData, null, 2), (err) => {
       if (err) {
         console.error("Failed to write to data.json:", err);
         res.status(500).send("Failed to save data");
